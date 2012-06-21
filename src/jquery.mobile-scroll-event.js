bd79821db587dec6db4d6d6594d7248590c6e834
Nicolas Brassard <nico@nitriques.com>
2012-06-21 19:02:15 -0400

    Added LICENSE and source files

---
+++ src/jquery.mobile-scroll-event.js
@@ -0,0 +1,6 @@
+/*
+*	Mobile Scroll Event v1.0 - jQuery plugin
+*
+*	Copyright (c) 2012 Deux Huit Huit (http://www.nitriques.com/open-source/)
+*	Licensed under the MIT (https://raw.github.com/DeuxHuitHuit/jQuery-sprite-animation/master/LICENSE.txt)
+*/
