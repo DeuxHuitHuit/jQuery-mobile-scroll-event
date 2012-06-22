679a836f3f6b215c8092137cdc28cc0630c31dab
Nicolas Brassard <nico@nitriques.com>
2012-06-22 00:22:23 -0400

    Added console as global

--- grunt.js
+++ grunt.js
@@ -51,7 +51,8 @@ module.exports = function(grunt) {
         lastsemic: true
       },
       globals: {
-        jQuery: true
+        jQuery: true,
+        console: true
       }
     },
     uglify: {},
